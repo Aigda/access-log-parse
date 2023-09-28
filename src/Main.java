dd470f02118a928b2aaea30dbe906c762f66da48
Aigda <aigda@internet.ru>
2023-09-28 20:55:16 +0300

    Базовые типы данных

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        //int firstNumber = 5;
+        //int secondNumber = 10;
+
+        int sumNumbers = firstNumber + secondNumber;
+        int subNumbers = firstNumber - secondNumber;
+        int multNumbers = firstNumber * secondNumber;
+        double quotientNumbers  = (double) firstNumber / secondNumber;
+
+        System.out.println("Были введены следующие числа : " + firstNumber + " и " + secondNumber);
+        System.out.println("Сумма чисел: " + sumNumbers);
+        System.out.println("Разность чисел: " + subNumbers);
+        System.out.println("Умножение чисел: " + multNumbers);
+        System.out.println("Частное чисел: " + quotientNumbers);
     }
 }
