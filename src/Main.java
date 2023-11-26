ebc10e022980091f31ef39b8525eb631e53821fb
aigda <aigda@internet.ru>
2023-11-26 13:40:07 +0300

    Exception included

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,55 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyRuntimeException {
 
+        String path = "C:\\Users\\asabanin\\Downloads\\access.log";
+        FileReader fileReader = null;
+        int iMinLength = 0;
+        int iMaxLength = 0;
+        boolean bFirstInit = true;
+        try {
+            fileReader = new FileReader(path);
+        } catch (FileNotFoundException e) {
+            throw new MyRuntimeException("Понятное сообщение об ошибке: " + e.getMessage());
+        }
+        BufferedReader reader =
+                new BufferedReader(fileReader);
+        String line;
+        int length = 0;
+        int iCount = 0;
+        while (true) {
+            try {
+                if (!((line = reader.readLine()) != null)) break;
+            } catch (IOException e) {
+                throw new MyRuntimeException("Понятное сообщение об ошибке: " + e.getMessage());
+            }
+            length = line.length();
+
+            if (length > 1024) {
+                throw new MyRuntimeException("В файле встречается строка длинее 1024 символов");
+            }
+
+            if (bFirstInit) {
+                bFirstInit = false;
+                iMaxLength = length;
+                iMinLength = length;
+            }
+
+            if (length > iMaxLength) iMaxLength = length;
+            if (length < iMinLength) iMinLength = length;
+
+            iCount++;
+        }
+
+        System.out.println("Число линий: " + iCount);
+        System.out.println("Длина самой длинной линии: " + iMaxLength);
+        System.out.println("Длина самой короткой линии: " + iMinLength);
+
+/*
         System.out.println("Введите первое число: ");
         int firstNumber = new Scanner(System.in).nextInt();
 
@@ -22,5 +69,8 @@ public class Main {
         System.out.println("Разность чисел: " + subNumbers);
         System.out.println("Умножение чисел: " + multNumbers);
         System.out.println("Частное чисел: " + quotientNumbers);
+
+
+ */
     }
 }
